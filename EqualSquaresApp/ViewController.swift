2a447716a9c555eda66bc131c97347b25b7f56e6
Oksana Tugusheva <gutuxa@Oksanas-MacBook-Pro-Old.local>
2021-03-23 17:34:58 +0300

    HW_2.1 equal shapes

---
+++ EqualSquaresApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EqualSquaresApp
+//
+//  Created by Oksana Tugusheva on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
